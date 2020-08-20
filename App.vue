3223e9fb58f94ed1d736fdecdd8a798709bc8491
vovak1978 <54435594+vovak1978@users.noreply.github.com>
2020-08-20 15:37:51 +0300

    add files
    mu first vuetask

---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="container">
+    <input v-model="message">
+   <button v-on:click="addlist" v-if="todoarray.length == 10 ?show = false :show=true">Add new todo</button> <br>    
+    </div>
+  <div class="container">
+  <ul>
+    <li v-for="(element, index) in todoarray" :key="index">{{element}} <button v-on:click="remove(index)" > Remove </button> </li>
+  </ul>
+  </div>
+    
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'App',
+  
+data() {
+  return {
+  message: '',  
+  todoarray: [],
+   }
+  },
+
+methods: {
+  addlist: function() {
+        console.log(this.message) 
+       this.todoarray.push(this.message);
+       this.message = ''
+       },
+  
+  remove: function(index) {
+        this.todoarray.splice(index,1)
+  }
+}
+}
+</script>
+
+<style>
+.container{
+  display: flex;
+ justify-content: center;
+}
+</style>
