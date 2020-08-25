2a78476a983d994edfa2e7d9b67da1ceb3494ea0
vovak1978 <54435594+vovak1978@users.noreply.github.com>
2020-08-25 19:04:35 +0300

    create parent and child

---
+++ task2/components/Frameworks/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>
+      {{element}}
+      <button @click="deletitem">Delete</button>
+
+    </span>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoItem',
+
+  props:{
+    element:{
+      type: String
+    }
+  },
+
+
+  methods: {
+    deletitem(){
+      this.$emit("deletitem", this.element)
+
+    }
+
+  }
+}
+</script>
